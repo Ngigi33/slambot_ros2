00e2c4bd5f4b73c3635b13b017bd68ac15665430
Peter Ndiba <ndibapeter4@gmail.com>
2024-09-28 19:08:05 +0300

    first commit

---
+++ src/slambot_description/launch/sim.launch.py
@@ -0,0 +1,63 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+
+    package_name='slambot_description'
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','robot_description.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    gazebo_params_file = os.path.join(get_package_share_directory('slambot_controllers'), 'config', 'gazebo_params.yaml')
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+             )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'diff_bot'],
+                        output='screen')
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+    # Launch them all!
+    return LaunchDescription([
+        # DeclareLaunchArgument(
+        #     'is_sim',
+        #     default_value='true',
+        #     description='Use sim time if true'),
+
+        rsp,
+        gazebo,
+        spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
+    ])
