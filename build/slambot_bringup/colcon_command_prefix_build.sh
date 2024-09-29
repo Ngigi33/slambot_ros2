60f6d47eed2f50c1f5224b3c6b9d81ff3bafe18f
Ngigi33 <albertngigi3@gmail.com>
2024-09-29 14:11:31 +0300

    Commit

---
+++ build/slambot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/einstein/Desktop/slambot_ros2/install/slambot_description/share/slambot_description/package.sh"
+. "/home/einstein/Desktop/slambot_ros2/install/slambot_firmware/share/slambot_firmware/package.sh"
